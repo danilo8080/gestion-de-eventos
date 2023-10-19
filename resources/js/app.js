5e797571c3a7ed805899c44113cf8832f4871460
bimbo512 <bimbo1229@gmail.com>
2023-10-18 21:11:05 -0500

    Agregando funcionalidad al front, mostrar registro y login

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,18 @@
-const loginForm = document.getElementById('login-form');
-const registerForm = document.getElementById('register-form');
-const registerButton = document.getElementById('register-button');
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.querySelector(".login");
+    const registerForm = document.querySelector(".registrar");
+    const showRegisterLink = document.querySelector(".botonLogin");
+    const showLoginLink = document.querySelector(".botonRegistro");
 
-registerButton.addEventListener('click', () => {
-    loginForm.style.display = 'block';
-    registerForm.style.display = 'none';
-    console.log("hola");
-});
+    showRegisterLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    });
 
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Aquí debes agregar la lógica para verificar las credenciales del usuario.
-    // Por ejemplo, puedes comparar los valores ingresados con una lista de usuarios válidos.
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Simulando una comprobación simple
-    if (username === 'usuario' && password === 'contraseña') {
-        alert('Inicio de sesión exitoso');
-    } else {
-        alert('Credenciales incorrectas');
-    }
-});
-
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Aquí debes agregar la lógica para registrar al usuario.
-    // Puedes guardar los datos en una base de datos, en una lista en memoria, o donde prefieras.
-    const newUsername = document.getElementById('new-username').value;
-    const newPassword = document.getElementById('new-password').value;
-
-    // Simulando un registro simple
-    alert(`Usuario registrado: ${newUsername}`);
+    showLoginLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        registerForm.style.display = "none";
+        loginForm.style.display = "block";
+    });
 });
