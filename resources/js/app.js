ae0f946ea23782758600bd6c297ce9de0f0c05fa
bimbo512 <bimbo1229@gmail.com>
2023-10-19 11:11:40 -0500

    Guardando los datos del registro/login en variables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,76 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.querySelector(".login");
     const registerForm = document.querySelector(".registrar");
-    const showRegisterLink = document.querySelector(".botonLogin");
-    const showLoginLink = document.querySelector(".botonRegistro");
+    const showRegisterLink = document.querySelector(".botonRegistro");
+    const showLoginLink = document.querySelector(".botonLogin");
 
+    const loginData = {
+        username: null,
+        password: null
+    };
+
+    const registerData = {
+        username: null,
+        apodo:null,
+        password: null
+    };
+
+    loginForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const userLogin = document.getElementById("userLogin").value;
+        const contraLogin = document.getElementById("contraLogin").value;
+
+        loginData.username = userLogin;
+        loginData.password = contraLogin;
+        console.log(loginData);
+
+        //código de inicio de sesión
+    });
+
+    registerForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const userRegister = document.getElementById("userRegister").value;
+        const apodoRegister = document.getElementById("apodoRegister").value;
+        const passwordRegister = document.getElementById("passwordRegister").value;
+
+        registerData.username = userRegister;
+        registerData.apodo = apodoRegister;
+        registerData.password = passwordRegister;
+        console.log(registerData);
+        //código de registro
+    });
+
+    ///////////Segunda opcion usando local storage/////////////////
+    /* ///////////////Login////////////
+    loginForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const userLogin = document.getElementById("userLogin").value;
+        const contraLogin = document.getElementById("contraLogin").value;
+
+        localStorage.setItem("loginUsername", userLogin);
+        localStorage.setItem("loginPassword", contraLogin);
+
+        // Resto del código de inicio de sesión
+    });
+    */
+    /*
+    registerForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const userRegister = document.getElementById("userRegister").value;
+        const apodoRegister = document.getElementById("apodoRegister").value;
+        const passwordRegister = document.getElementById("passwordRegister").value;
+
+        localStorage.setItem("registerUsername", userRegister);
+        localStorage.setItem("registerApodo", apodoRegister);
+        localStorage.setItem("registerPassword", passwordRegister);
+
+        // Resto del código de registro
+    });
+    */
     showRegisterLink.addEventListener("click", function(e) {
         e.preventDefault();
         loginForm.style.display = "none";
@@ -16,3 +83,4 @@ document.addEventListener("DOMContentLoaded", function() {
         loginForm.style.display = "block";
     });
 });
+
